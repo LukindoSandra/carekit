8c2e8dffed80aedfd9346dc28e242b94d1bcd2df
LukindoSandra <sandramaryanne9@gmail.com>
2020-02-24 10:54:52 +0300

    added the hide and show

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#what1").click(function(){
+      $("#p1").toggle();
+      $("#img1").toggle();
+    });
+    $("#what2").click(function(){
+        $("#p2").toggle();
+        $("#img2").toggle();
+      });
+      $("#what3").click(function(){
+        $("#p3").toggle();
+        $("#img3").toggle();
+      });
+  });
