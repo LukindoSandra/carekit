c729eb269fbe78412ee6e5da498a4143dd8cf78e
LukindoSandra <sandramaryanne9@gmail.com>
2020-02-25 09:21:37 +0300

    made changes on my JS and HTML

--- script.js
+++ script.js
@@ -12,6 +12,85 @@ $(document).ready(function(){
         $("#img3").toggle();
       });
   });
-  function myFunction() {
-    alert("Thank you for reaching out to Delani studios we will get back to you shortly");
+$().hover(
+  function(){
+    $("#text1").show();
+  },
+  function(){
+    $("#desktop").hide();
   }
+)
+$().hover(
+  function(){
+    $("#text2").show();
+  },
+  function(){
+    $("#color").hide();
+  }
+)
+$().hover(
+  function(){
+    $("#text3").show();
+  },
+  function(){
+    $("#ontario").hide();
+  }
+)
+$().hover(
+  function(){
+    $("#text4").show();
+  },
+  function(){
+    $("#blank").hide();
+  }
+)
+$().hover(
+  function(){
+    $("#text5").show();
+  },
+  function(){
+    $("#wishes").hide();
+  }
+)
+$().hover(
+  function(){
+    $("#text6").show();
+  },
+  function(){
+    $("#icons").hide();
+  }
+)
+$().hover(
+  function(){
+    $("#text7").show();
+  },
+  function(){
+    $("#burned").hide();
+  }
+)
+$().hover(
+  function(){
+    $("#text8").show();
+  },
+  function(){
+    $("#giraffe").hide();
+  }
+)
+$(document).ready(function(){
+  var $form = $('#formAdd');
+  $form.submit(function(){
+      var id= $("#id").val();
+      if (id.length < 12) {
+          alert("INPUT ERROR");
+          return false;
+      }
+
+      $.post($form.attr('action'), $(this).serialize(), function(response){
+          alert("DATA SUCCESSFULLY ADDED");
+      },'json');
+      return false;
+  });
+});
+function myFunction() {
+  alert("Thank you for contacting Delani studios we shall get back to you shortly!");
+}
